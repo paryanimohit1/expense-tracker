ae12a9bac202e831fe7c4a3881c150075a9dbc09
Mohit Paryani <>
2019-09-07 23:30:47 +0530

    Update FirebaseConfiguration.js

--- src/FirebaseConfiguration.js
+++ src/FirebaseConfiguration.js
@@ -1,6 +1,6 @@
 (function () {
     var firebaseConfig = {
-        // Your Firebase project configuration
+		// Your Firebase project configuration
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
